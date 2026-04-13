5d12a6170bde72709f8c89a6b8489c4d08b2c5ee
st10477929 <st10477929@rcconnect.edu.za>
2026-04-13 15:29:57 +0200

    Update AudioPlayer.cs

--- CyberSecurityBot/AudioPlayer.cs
+++ CyberSecurityBot/AudioPlayer.cs
@@ -1,36 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Media;
 using System.IO;
 
 namespace CyberSecurityBot
 {
-    public static class AudioPlayer
-    {
-        public static void PlayGreeting()
-        {
-            try
-            {
-                string audioPath = Path.Combine(
-                    AppDomain.CurrentDomain.BaseDirectory,
-                    "Assets",
-                    "greeting.wav"
-                );
+    using System;
+    using System.Media;
+    using System.IO;
 
-                if (File.Exists(audioPath))
-                {
-                    SoundPlayer soundPlayer = new SoundPlayer(audioPath);
-                    soundPlayer.PlaySync();
-                }
-                else
-                {
-                    Console.Beep(600, 400);
-                    Console.Beep(800, 400);
-                }
-            }
-            catch
+    namespace CyberSecurityBot
+    {
+        // Handles all audio playback functionality 
+        public static class AudioPlayer
+        {
+            public static void PlayGreeting()
             {
-                Console.WriteLine("[!] Voice playback unavailable.");
+                try
+                {
+                    string audioPath = Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        "Assets",
+                        "greeting.wav"
+                    );
+
+                    if (File.Exists(audioPath))
+                    {
+                        SoundPlayer soundPlayer = new SoundPlayer(audioPath);
+                        soundPlayer.PlaySync();
+                    }
+                    else
+                    {
+                        Console.Beep(600, 400);
+                        Console.Beep(800, 400);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Voice playback unavailable.");
+                }
             }
         }
     }
 }
+        
+   
